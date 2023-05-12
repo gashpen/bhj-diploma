db35b04a0ad3cbdcd91ee8cc3bdc6d5634f15871
gashpen <constantinent@mail.ru>
2023-05-12 14:38:53 +0300

    createRequest_2

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -4,15 +4,12 @@ const createRequest = (options = {}) => {
   const formData = new FormData();
   const responseServer = options.callback;
   
-  for (elem in options.data) {
-
-    if(options.method === 'GET'){
-      xhr.open(options.method, options.data[elem]);
-    } else {
-      xhr.open(options.method, options.url);
-    }
-  };
-
+  if(options.method === 'GET'){
+    xhr.open(options.method, options.data);
+  } else {
+    xhr.open(options.method, options.url);
+  }
+  
   options.method === 'GET' ?  xhr.send() : xhr.send(formData);
 
   xhr.responseType = 'json';
